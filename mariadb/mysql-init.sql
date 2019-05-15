4567e285f65ed821481cccb463f5611acd45f048
Donald R Elsborg <elsborg@colorado.edu>
2019-05-15 15:11:43 -0600

    changes to default db to utf8mb4

--- mariadb/mysql-init.sql
+++ mariadb/mysql-init.sql
@@ -1,2 +1,2 @@
-CREATE DATABASE vitrodb CHARACTER SET utf8;
+CREATE DATABASE vitrodb CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 GRANT ALL ON vitrodb.* TO 'vivo'@'%' IDENTIFIED BY 'las_vegas';
