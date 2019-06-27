dc8da6bcdbbaed6924757aa50206ddba8a4a3545
Ralph O'Flinn <roflinn@uab.edu>
2019-06-27 03:50:50 -0500

    version updates for solr and mariadb, change some wording back to original, use git instead of tar

--- mariadb/mysql-init.sql
+++ mariadb/mysql-init.sql
@@ -1,2 +1,2 @@
-CREATE DATABASE vitrodb CHARACTER SET utf8;
-GRANT ALL ON vitrodb.* TO 'vivo'@'%' IDENTIFIED BY 'las_vegas';
+CREATE DATABASE vitrodb CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+GRANT ALL ON vitrodb.* TO 'vitrodbUsername'@'localhost' IDENTIFIED BY 'vitrodbPassword';
