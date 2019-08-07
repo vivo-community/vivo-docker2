a065e2b56730cfd1ae9b5a1b022b435e94e590d3
Ralph O'Flinn <roflinn@users.noreply.github.com>
2019-08-06 20:05:40 -0500

    Update mysql-init.sql
    localhost to %

--- mariadb/mysql-init.sql
+++ mariadb/mysql-init.sql
@@ -1,2 +1,2 @@
 CREATE DATABASE vitrodb CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-GRANT ALL ON vitrodb.* TO 'vitrodbUsername'@'localhost' IDENTIFIED BY 'vitrodbPassword';
+GRANT ALL ON vitrodb.* TO 'vitrodbUsername'@'%' IDENTIFIED BY 'vitrodbPassword';
