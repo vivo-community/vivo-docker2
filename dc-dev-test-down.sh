84a33ca6c580565e798db59bbeef15952e80dc8d
Ralph O'Flinn <roflinn@hotmail.com>
2019-08-19 16:26:55 -0500

    dev script fix

--- dc-dev-test-down.sh
+++ dc-dev-test-down.sh
@@ -2,7 +2,7 @@
 
 docker-compose down
 docker rmi vivo-docker2_vivo vivo-docker2_solr vivo-docker2_mariadb
-docker rmi solr mysql tomcat
+docker rmi solr mariadb tomcat
 docker volume ls
 docker volume prune
 docker volume ls
