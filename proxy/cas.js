95b27e7490564a41d437dce74ac2628435da2b9d
Justin Merz <jrmerz@gmail.com>
2019-08-21 17:04:39 -0700

    adding UCD CAS authentication

---
+++ proxy/cas.js
@@ -0,0 +1,40 @@
+const CASAuthentication = require('cas-authentication');
+
+const AGENT_DOMAIN = process.env.CAS_AGENT_DOMAIN;
+
+let cas = new CASAuthentication({
+  cas_url     : process.env.CAS_URL,
+  service_url : process.env.APP_URL
+});
+
+function init(app, proxy) {
+
+  return (req, res) => {
+    console.log('CAS Service: starting CAS redirection');
+
+    req.query.returnTo = process.env.APP_URL+'/vivo/loginExternalAuthReturn';
+    cas.service_url = process.env.APP_URL+'/vivo/loginExternalAuthReturn';
+
+    cas.bounce(req, res, async () => {
+      console.log('CAS Service: CAS redirection complete');
+
+      let username = '';
+      if( cas.session_name && req.session[cas.session_name] ) {
+        username = req.session[cas.session_name]+'@'+AGENT_DOMAIN;
+      }
+
+      if( username ) {
+        req.headers['ucd-cas-id']=username;
+        console.log('CAS Service: CAS login success: '+username);
+      } else {
+        console.error('CAS Service: CAS login failure.  No username found');
+      }
+
+      proxy.web(req, res, { target: `http://vivo:8080/vivo/loginExternalAuthReturn` });
+    });
+  };
+}
+
+
+
+module.exports = init;
