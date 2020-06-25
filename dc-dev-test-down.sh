4f25137b3e6de2775b93830604efbff6dbb5b18e
Garrett Armstrong <garrett.w.armstrong@gmail.com>
2020-06-25 13:13:47 -0400

    specifies which volumes to docker volume rm

--- dc-dev-test-down.sh
+++ dc-dev-test-down.sh
@@ -4,5 +4,5 @@ docker-compose down
 docker rmi vivo-docker2_vivo vivo-docker2_solr vivo-docker2_mariadb
 docker rmi solr mariadb tomcat
 docker volume ls
-docker volume prune
+docker volume rm vivo-docker2_maria-data vivo-docker2_solr-data
 docker volume ls
