11adb092ef601757a19c5833dd55f3d7163c35f0
Ben <mbgross@wustl.edu>
2020-10-01 10:45:53 -0600

    Add docker for sprint-i18n branch testing (#19)
    * Setup docker to test VIVO-1848 using TDB
    * Point docker to sprint-i18n branches
    * Update solrconfig.xml to latest version

--- mariadb/mysql-init.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE vitrodb CHARACTER SET utf8;
-GRANT ALL PRIVILEGES ON vitrodb.* TO 'vitrodbUsername'@'%' IDENTIFIED BY 'vitrodbPassword';
-GRANT ALL PRIVILEGES ON vitrodb.* TO 'vitrodbUsername'@'localhost' IDENTIFIED BY 'vitrodbPassword';
-FLUSH PRIVILEGES;
-
-
