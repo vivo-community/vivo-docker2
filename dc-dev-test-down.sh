f6f640ed1a9101daabeca1ac4c8a5c29df8625e8
Garrett Armstrong <garrett.w.armstrong@gmail.com>
2023-10-31 13:07:09 -0400

    current main repo minus frills

--- dc-dev-test-down.sh
+++
@@ -1,8 +0,0 @@
-# WARNING! - Only for development testing! May remove items outside of vivo-docker2 scope.
-
-docker-compose down
-docker rmi vivo-docker2_vivo vivo-docker2_solr vivo-docker2_mariadb
-docker rmi solr mariadb tomcat
-docker volume ls
-docker volume rm vivo-docker2_maria-data vivo-docker2_solr-data
-docker volume ls
